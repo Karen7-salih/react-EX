df9ee810035adf5e0905bbc0bd4c9ecf107527d4
Karen7-salih <Karen7-salih.salihkaren345@gmail.com>
2025-02-10 16:18:14 +0200

    targil 2

--- src/App.js
+++ src/App.js
@@ -11,15 +11,35 @@ function UserList(props){
   </ul>
 }
 
+function ProductList(props){
+  console.log(props.productList)
+  return <ul style={{backgroundColor: "green" , listStyleType:"none"}}>
+    {props.productList.map((item , index) => { 
+      return <li key={index} > {item.name} {item.price} </li>
+    })}
+  </ul>
+
+
+
+
+}
+
 function App() {
 
   const myList = [{id : 1 , name : "kareen" , email : "salihkaren345@gmail.com"} ,
     {id : 2 , name : "kareenS" , email : "salihakfsn345@gmail.com"}
   ]
 
+  const myList2 = [{id : 1 , name : "mascar" , price : 250} ,
+    {id : 2 , name : "concealer" , price : 500}
+  ]
+
   return (
     <div className="App">
       <UserList userList={myList}/>
+      <ProductList productList={myList2}/>
+
+
 
     </div>
   );
